9ae060754455886ddcbaa85f713e05b5c01b0b9c
JorgeVargas03 <joluvargaspa@ittepic.edu.mx>
2025-05-13 09:33:13 -0700

    sms

--- controllers/SMS.js
+++ controllers/SMS.js
@@ -3,8 +3,7 @@ const token = process.env.AUTH_TOKEN;
 
 const client = require('twilio')(acc_Id, token);
 
-client.messages
-    .create({
-        to: '[HandsetNumber]'
-    })
-    .then(message => console.log(message.sid));
+client.verify.v2.services("VAd8e2829df7959ec56172a2199402288b")
+    .verifications
+    .create({ to: '+523112415972', channel: 'sms' })
+    .then(verification => console.log(verification.sid));
